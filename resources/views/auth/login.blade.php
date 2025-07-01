00b867c30abee6caf89babe1b0890719b350e9ed
iksan <iksansansansan@gmail.com>
2025-07-02 00:46:21 +0700

    feat: add login and register logic

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
+                <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url({{ asset ('assets/images/hero_bg_2.jpg') }});" data-aos="fade" data-stellar-background-ratio="0.5">
+                    <div class="container">
+                        <div class="row align-items-center justify-content-center text-center">
+                        <div class="col-md-10">
+                            <h1 class="mb-2">Log In</h1>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="site-section">
+                    <div class="container">
+                        <div class="row">
+                        <div class="col-md-12" data-aos="fade-up" data-aos-delay="100">
+                            <h3 class="h4 text-black widget-title mb-3">Login</h3>
+                            <form method="POST"action="{{ route('login') }}" class="form-contact-agent">
+                                @csrf
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            </div>
+                            @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            </div>
+                            @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                            <div class="form-group">
+                                <input type="submit" id="phone" name="submit" class="btn btn-primary" value="Login">
+                            </div>
+                            </form>
+                        </div>
+                        
+                        </div>
+                    </div>
+                   </div>
+
+
+                {{-- <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
@@ -65,9 +106,5 @@
                             </div>
                         </div>
                     </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                </div> --}}
 @endsection
